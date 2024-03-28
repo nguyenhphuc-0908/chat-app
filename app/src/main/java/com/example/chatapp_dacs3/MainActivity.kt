7c3c03d58abc0dbbd7d78dfb41a4b8bf01a3c344
i_PhucNguyen <115609761+phuc0908@users.noreply.github.com>
2024-03-28 09:47:43 +0700

    Update MainActivity.ktfgf

--- app/src/main/java/com/example/chatapp_dacs3/MainActivity.kt
+++ app/src/main/java/com/example/chatapp_dacs3/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Rangj Thie HEHEHEHEH fdf eur nawng")
-                    Greeting("ddsdsddd")
+                    Greeting("Phuc
 
                 }
             }
